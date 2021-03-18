528e0d5c53402e16c783da83a348fa298afa6214
mikedsosa <71150678+mikedsosa@users.noreply.github.com>
2021-03-18 09:12:27 -0600

    Update SignUpInfo.cs

--- ScheduleTest/Models/SignUpInfo.cs
+++ ScheduleTest/Models/SignUpInfo.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-
+// the best
 namespace ScheduleTest.Models
 {
     public class SignUpInfo
